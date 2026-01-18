bfe4f893bf9871ce04431540792ffc4eededff03
allen <allenushka2005@gmail.com>
2026-01-18 18:54:25 +0800

    Добавьте файлы проекта.

---
+++ laborat1/laborat1.cpp
@@ -0,0 +1,56 @@
+﻿
+#define GLEW_DLL
+#define GLFW_DLL
+
+#include <GL/glew.h>     
+#include <GLFW/glfw3.h>   
+#include <stdio.h>       
+
+int main() {
+    if (!glfwInit()) {
+        fprintf(stderr, "ERROR: could not start GLFW3.\n");
+        return 1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 1);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    // glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_FALSE);
+    // glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(512, 512, "mainwindow", NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    GLenum ret = glewInit();
+    if (GLEW_OK != ret) {
+        fprintf(stderr, "Error: %s\n", glewGetErrorString(ret));
+        return 1;
+    }
+
+    while (!glfwWindowShouldClose(window)) {
+
+        glClearColor(0.2, 0.3, 0.4, 0.0);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glBegin(GL_TRIANGLES);
+
+        glColor3f(0.5, 0.5, 0.0);
+
+        glVertex2f(0.0, 1.0);    
+        glVertex2f(-1.0, -1.0); 
+        glVertex2f(1.0, -1.0);  
+
+        glEnd();
+
+        glfwSwapBuffers(window);
+
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
